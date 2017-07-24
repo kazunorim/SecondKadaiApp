afb5210470e66ca5377a4e594882d56ede52b826
kazunorim <jam0.3t.bell-ndrp@softbank.ne.jp>
2017-07-24 12:02:44 +0900

    1つ目の画面で入力された名前を使って、「こんにちは、名前さん」と表示する 以外のところまではできました。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        resultViewController.x = 1
+    }
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 
 }
 
